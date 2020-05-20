89c34c0b699566e875fdaff9e19918a07d7a38d2
Luke Flannery <lukeflannery@Lukes-MBP-2.broadband>
2020-05-20 09:28:29 +0100

    geoJSON format, refactoring needed to create multiple instances

--- index.js
+++ index.js
@@ -3,33 +3,51 @@
 // PositionCount = 200 points
 
 const randomGeoPoints = (centrePoint, radius, positionCount) => {
-  const points = [];
+  // currently pushing all 200 points into that one array, needs refactoring to create a new object instance each time.
+  const geoJSON = {
+    name: "NewFeatureType",
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Point",
+        geometry: {
+          type: "Point",
+          coordinates: [],
+        },
+        properties: null,
+      },
+    ],
+  };
+  // iterates through given number of points,assigns each a coordinate, and pushes to coordinate array
   for (let i = 0; i < positionCount; i++) {
-    points.push(generateRandomPoint(centrePoint, radius));
+    geoJSON.features[0].geometry.coordinates.push(
+      generateRandomPoint(centrePoint, radius)
+    );
   }
-  return points;
+  return geoJSON;
 };
 
+// calculations neccessary to generate random coordinates
 const generateRandomPoint = (centrePoint, radius) => {
-  const longitude = centrePoint.lng;
-  const latitude = centrePoint.lat;
+  const centreLong = centrePoint.lng;
+  const centreLat = centrePoint.lat;
 
   // Convert Radius from meters to degrees.
   const radiusInDegrees = radius / 111300;
-
   const u = Math.random();
   const v = Math.random();
-
   const w = radiusInDegrees * Math.sqrt(u);
   const t = 2 * Math.PI * v;
   const x = w * Math.cos(t);
   const y = w * Math.sin(t);
+  const xp = x / Math.cos(centreLat);
 
-  const xp = x / Math.cos(latitude);
+  const latAndLong = (y + centreLat, xp + centreLong);
 
-  return { lat: y + latitude, lng: xp + longitude, time: currentTime() };
+  return latAndLong;
 };
 
+// current time with user friendly format - can be refactored to be a property of each 'point' on JSON
 const currentTime = () => {
   const dateNow = new Date();
   const hrs = dateNow.getHours();
@@ -39,6 +57,9 @@ const currentTime = () => {
   return fullTime;
 };
 
-// console.log(randomGeoPoints({ lat: 53.478056, lng: -2.245833 }, 1500, 200));
+// takes a central points coordinates, a radius and a number of points
+console.log(
+  JSON.stringify(randomGeoPoints({ lat: 53.478056, lng: -2.245833 }, 1500, 200))
+);
 
 module.exports = { randomGeoPoints, generateRandomPoint, currentTime };
